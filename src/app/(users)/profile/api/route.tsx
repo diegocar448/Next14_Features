0839b712eebc88fee67d0ba4e32527a554ec1794
Diego Cardoso <diegocar448@hotmail.com>
2024-09-18 17:35:34 -0300

    Cache não deixar o cache em modo de producao

--- src/app/(users)/profile/api/route.tsx
+++ src/app/(users)/profile/api/route.tsx
@@ -1,20 +1,27 @@
 import { NextRequest } from "next/server"
-import { headers } from "next/headers"
+import { headers, cookies } from "next/headers"
 
 export async function GET(request: NextRequest){   
 
     const requestHeaders = new Headers(request.headers)
     const headerList = headers()
 
+    cookies().set('token', 'asdfsa')
+    const theme = request.cookies.get('theme')
+
     console.log(requestHeaders.get('Authorization'))
     console.log(headerList.get(`Authorization`))
+
+    console.log(theme)
+    console.log(cookies().get('token'))
 
     //return new Response('Profile Route')
     return new Response(
         '<h1>Profile Route</h1>',
         {
             headers:{
-                "Content-Type": "text/html"
+                "Content-Type": "text/html",
+                "Set-Cookie": "theme=dark"
             }
         }
     )
