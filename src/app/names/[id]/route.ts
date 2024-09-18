4538ad58170208d624577c17a53ea3efe631a80c
Diego Cardoso <diegocar448@hotmail.com>
2024-09-18 15:04:38 -0300

    ManipulandoRotasPatch

--- src/app/names/[id]/route.ts
+++ src/app/names/[id]/route.ts
@@ -1,7 +1,7 @@
 import { data } from "../data";
 
 export async function GET(
-    _request,
+    _request: Request,
     { params }: {params: {id: string}}
 ){
 
@@ -9,4 +9,19 @@ export async function GET(
 
 
     return Response.json(name)
+}
+
+export async function PATCH(
+    request: Request,
+    { params }: {params: {id: string}}
+){
+
+    const body = await request.json()
+    const { text } = body
+    const index = data.findIndex(
+        user => user.id === parseInt(params.id)
+    )
+    data[index].name = text
+
+    return Response.json(data)
 }
