13799f968d414555ccb0859ef77e6788f81da639
Diego Cardoso <diegocar448@hotmail.com>
2024-09-18 16:22:28 -0300

    UrlQueryParams

--- src/app/names/route.ts
+++ src/app/names/route.ts
@@ -1,7 +1,15 @@
+import { NextRequest } from 'next/server'
 import { data } from './data'
 
-export async function GET(){
-    return Response.json(data)
+export async function GET(request: NextRequest){
+
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('query')
+    
+    const filteredNames = query ? data.filter(name => name.name.toLowerCase().includes(query.toLocaleLowerCase()))
+                                : data                                
+
+    return Response.json(filteredNames)
 }
 
 export async function POST(request: Request){
