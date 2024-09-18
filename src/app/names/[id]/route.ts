9f4019ca185e644c25fa976caace004e5ebe09dc
Diego Cardoso <diegocar448@hotmail.com>
2024-09-18 14:53:34 -0300

    Rotas Dinamicas

---
+++ src/app/names/[id]/route.ts
@@ -0,0 +1,12 @@
+import { data } from "../data";
+
+export async function GET(
+    _request,
+    { params }: {params: {id: string}}
+){
+
+    const name = data.find(name => name.id === parseInt(params.id))
+
+
+    return Response.json(name)
+}
